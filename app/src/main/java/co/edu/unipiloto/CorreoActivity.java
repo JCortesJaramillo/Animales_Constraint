5382af6cbe0534d6922a4f81946beaad353f768d
JCortesJaramillo <juandavidmurray30@gmail.com>
2025-03-05 10:15:14 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/CorreoActivity.java
@@ -0,0 +1,12 @@
+package co.edu.unipiloto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CorreoActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_correo);
+    }
+}
